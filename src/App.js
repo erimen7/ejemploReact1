75ba6998735717e9df0bc3bb5d9b8821e606fc01
Erick Sierra <erimensc@gmial.com>
2024-11-05 12:35:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Form from './components/Form';
+import Votacion from './components/Votacion'
+import imaga1 from './img/a1.jpg'
+import imaga2 from './img/a2.jpg'
+import imaga3 from './img/a3.jpg'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <p className='Parrafo'>Hola mundo.. </p>
+      <h1>es un titulo</h1>
+      <Form></Form>
+      <section className='ordenar'>
+      <Votacion titulo="La votacion para imagenes antiguas" descripcion="Es una imagen del siglo XVIII de un autor desconocido" imagenk={imaga1}></Votacion>
+      <Votacion titulo="La votacion para peliculas" descripcion="Pelicula de animatronicos " imagenk={imaga2}></Votacion>
+      <Votacion titulo="La votacion para caricaturas" descripcion="una imagen de caricatura" imagenk={imaga3}></Votacion>
+      </section>
+  </>
   );
 }
 
